4cc423491811bd80e90a1c0e6ea0e6cd3ca88e2d
think11723 <pratikwaghral711143@gmail.com>
2026-07-10 01:03:47 +0530

    Testing github extension

--- NetflixGPT/src/App.jsx
+++ NetflixGPT/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 
 
 function App() {
-  return <h1 className="font-bold flex justify-center">working</h1>
+  return <h1 className="font-bold flex justify-center">working .</h1>
 }
 
 export default App
